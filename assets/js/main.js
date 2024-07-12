3456fb550c8150ba49c13379ef35b36b2ff6134b
Carlos Santana <carlos.santanag@live.cl>
2024-07-12 19:20:08 -0400

    version 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,29 @@
-$(document).ready(function(){
 
-    $("p").dblclick(function(){
-    $(this).hide("slow");
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
     });
 
-    $("p").click(function(){
-        $(this).css({
-        "color": "red"
-        });
+    $("#enviarCorreo").click(function() {
+        alert("El correo fue enviado correctamente...");
+    });
+
+    $(".card-body").click(function() {
+        $(".card-text").toggle();
     });
 });
+
+$(document).ready(function(){
+    $("#spoon1").dblclick(function(){
+        $(this).css({"color": "red"});
+    });
+});
+
+$(document).ready(function(){
+    $("#spoon2").dblclick(function(){
+        $(this).css({"color": "red"});
+    });
+});
+
+
